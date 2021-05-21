39219f029a94e52d67bcbce7bdb30bf0c7feab98
sblum55 <healysa55@gmail.com>
2021-05-21 16:46:20 -0400

    navbar and app.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,22 @@ import { Route, Redirect } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
+import NavBar from './components/NavBar'
+import SignUp from './pages/SignUp'
+import userEvent from '@testing-library/user-event';
+
 function App() {
   return (
     <div className="App">
+      <NavBar />
+
+      <Route exact path = '/signup' render={() => {
+        if (user.id) {
+          return <Redirect to = '/'/>
+        } else {
+          return<SignUp />
+        }
+      }} />
       
     </div>
   );
